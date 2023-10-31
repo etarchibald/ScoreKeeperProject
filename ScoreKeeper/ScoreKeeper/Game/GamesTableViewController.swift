cb4605d18027b1d3ddce587ce84747d7308c4e0f
etarchibald <ethanarchibald@Veronica.local>
2023-10-31 10:09:10 -0600

    edit game rows

--- ScoreKeeper/ScoreKeeper/Game/GamesTableViewController.swift
+++ ScoreKeeper/ScoreKeeper/Game/GamesTableViewController.swift
@@ -9,30 +9,40 @@ import UIKit
 
 class GamesTableViewController: UITableViewController {
     
-    var game = [Game]()
+    var games = [Game]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        game.append(Game(title: "Egyptian Rat Screw", currentWinner: "Player 4", players: [Player(name: "Player 4", score: 38)]))
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return game.count
+        return games.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "gameCell", for: indexPath) as! GameTableViewCell
 
-        let game = game[indexPath.row]
+        let game = games[indexPath.row]
         
         cell.update(with: game)
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            games.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -43,9 +53,7 @@ class GamesTableViewController: UITableViewController {
 
 extension GamesTableViewController: AddGameDelegate {
     func addGame(_ game: Game) {
-        self.game.append(game)
+        self.games.append(game)
         tableView.reloadData()
     }
-    
-    
 }
